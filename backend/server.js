5c2491c007cc2f371c2f4dcb34bfe1ba28d91a4a
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-09-23 17:17:04 +0300

    Some little changes in package.json and server.js and project ready to development

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import movieRoutes from "./routes/movie.routes.js";
@@ -12,6 +13,9 @@ import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -20,7 +24,13 @@ app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
-const PORT = ENV_VARS.PORT;
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
